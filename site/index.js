81dc85b3def1ae4b8c0715c6ed990fc93d2f8d45
Clément Gauthier <57112933+clem2004@users.noreply.github.com>
2021-04-03 18:51:26 -0400

    Update index.js

--- site/index.js
+++ site/index.js
@@ -101,7 +101,7 @@ function GenerateHTMLCell(title, subtitle, image, i, cellClass) {
         return `<cell id = ${i} class="${cellClass}"><img src="${image}" height="80px"/><description><cell-title>${title}</cell-title><cell-subtitle>${subtitle}</cell-subtitle></description></cell>`
     } else if (isEmoji(image) != true) {
         // IMAGE IS IMAGE NAME
-        return `<cell id = ${i} class="${cellClass}"><img src="https://clem2004.github.io/site/images/${image}.png" height="80px"/><description><cell-title>${title}</cell-title><cell-subtitle>${subtitle}</cell-subtitle></description></cell>`
+        return `<cell id = ${i} class="${cellClass}"><img src="https://clem2004.github.io/files/images/${image}.png" height="80px"/><description><cell-title>${title}</cell-title><cell-subtitle>${subtitle}</cell-subtitle></description></cell>`
     } else {
         // IMAGE IS EMOJI
         return `<cell id = ${i} class="${cellClass}"><emoji>${image}</emoji><description><cell-title>${title}</cell-title><cell-subtitle>${subtitle}</cell-subtitle></description></cell>`
